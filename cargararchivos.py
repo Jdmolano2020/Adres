d69c58d9e0c3d960595b88862536b4f0071b5686
franklin-cardona <franklin.cardona@adres.gov.co>
2025-03-20 10:07:02 -0500

    Update cargararchivos.py
    dtype a archivo 2.ADR_SupplierAddressImportTemplate_ADR.xlsm

--- cargararchivos.py
+++ cargararchivos.py
@@ -1,20 +1,30 @@
+"""
+Leer Archivos Replicas de información descargadas de los ambientes
+	• ADR_RP (Oracle), Archivos Descargada (DINAMICS), OCI_TERCEROS 
+    (Construcción a 31 de enero-"alterno")
+Leer Archivo con los números de identificación y concepto de los terceros a Buscar
+"""
 import pandas as pd
 
 # Cargar el archivo Excel
 file_path = '1.ADR_SupplierImportTemplate_ADR.xlsm'
-df_ADR_Supplier = pd.read_excel(file_path, sheet_name="POZ_SUPPLIERS_INT", header=3 )
+df_ADR_Supplier = pd.read_excel(
+    file_path, sheet_name="POZ_SUPPLIERS_INT", dtype=str, header=3)
 
 file_path = '2.ADR_SupplierAddressImportTemplate_ADR.xlsm'
-df_Address = pd.read_excel(file_path, sheet_name="POZ_SUPPLIER_ADDRESSES_INT", header=3)
+df_Address = pd.read_excel(
+    file_path, sheet_name="POZ_SUPPLIER_ADDRESSES_INT", dtype=str, header=3)
 
 file_path = "7.ADR_SupplierBankAccountImportTemplate_ADR.xlsm"
 # Cargar las hojas en DataFrames
-df_payees = pd.read_excel(file_path, sheet_name="IBY_TEMP_EXT_PAYEES", dtype=str, header=3)
-df_bank_accts = pd.read_excel(file_path, sheet_name="IBY_TEMP_EXT_BANK_ACCTS ", dtype=str, header=3)
+df_payees = pd.read_excel(
+    file_path, sheet_name="IBY_TEMP_EXT_PAYEES", dtype=str, header=3)
+df_bank_accts = pd.read_excel(
+    file_path, sheet_name="IBY_TEMP_EXT_BANK_ACCTS ", dtype=str, header=3)
 
 file_path = 'TerceroBusca.xlsx'
 df_tercerosb = pd.read_excel(file_path, dtype={
-    'NumeroDocumento': str, 'ConceptoPagoX': str,})
+    'NumeroDocumento': str, 'ConceptoPagoX': str, })
 
 file_path = 'OCI_TERCEROS.xlsx'
 df_terceros_v1 = pd.read_excel(file_path, dtype={
